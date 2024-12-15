d7ce725103e3fab969d791dcdcd797b644eeb493
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2024-12-15 21:00:01 +0545

    list

---
+++ WEEK 1/Day3/01_list.py
@@ -0,0 +1,16 @@
+use_list=['Alex','Raju','Shyam']
+print(use_list)
+print(use_list[1])
+print(use_list[2])
+
+
+use_list.append('prabesh')
+print(use_list)
+
+
+use_list.remove('Raju')
+print(use_list)
+
+
+use_list[2]='shyam'
+print(use_list)
