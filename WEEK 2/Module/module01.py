8c801ba021dc96779512e090483638111fdc5da9
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-01-03 21:53:29 +0545

    modules

---
+++ WEEK 2/Module/module01.py
@@ -0,0 +1,14 @@
+# collection of variable class is called module
+# like library pre defined and user define module
+
+# reuse of module like import the python file always use python file
+
+# call the module and create the module
+
+def sum(a,b):
+    c=a+b
+    return c
+def mul(a,b):
+    c=a*b
+    return c
+
---
+++ WEEK 2/Module/modules.py
@@ -0,0 +1,4 @@
+import module01 as m1
+
+
+print(m1.sum(10,10))
