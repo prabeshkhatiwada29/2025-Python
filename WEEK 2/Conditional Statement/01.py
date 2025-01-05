dd3e0ba2a823edacdeb51deed4c0e1267b76349f
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-01-05 21:41:30 +0545

    conditional statement

---
+++ WEEK 2/Conditional Statement/01.py
@@ -0,0 +1,9 @@
+# control statement are statement which control and follow
+
+number=int (input("enter the number"))
+
+if number >5:
+    print("number is greater than 5")
+
+else:
+    print("Not greater") 
