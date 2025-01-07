d9b2efdedf7fff0ca8eba1b886880571d52932ca
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-01-07 20:03:40 +0545

    oops

---
+++ WEEK 2/oops pratice/01.py
@@ -0,0 +1,11 @@
+class Car :
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year =year
+my_car =Car("Nano","Corolla",1999)
+print(my_car.brand)
+
+
+my_car=Car("pulser","rajako pala ko",1994)
+print(my_car.model)
