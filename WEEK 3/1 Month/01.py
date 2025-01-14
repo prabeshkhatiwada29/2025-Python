1451570480c05160ba168933b187bf5b7c842c8d
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-01-14 12:55:56 +0545

    1 month pratice

---
+++ WEEK 3/1 Month/01.py
@@ -0,0 +1,6 @@
+name= input("Enter your name=")
+x=0
+while x<5:
+    print("Hello",name)
+    x=x+1
+    
---
+++ WEEK 3/1 Month/02.py
@@ -0,0 +1,5 @@
+# to display the number from 1 to 10
+x=1
+while x<=10:
+    print(x)
+    x=x+1
