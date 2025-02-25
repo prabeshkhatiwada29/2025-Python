f50e478eba90e1feddd3f5b80b1bafaa6f08659e
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-02-25 21:42:07 +0545

    pratice

---
+++ OOPS ONLY/exceptionhandaling/01.py
@@ -0,0 +1,8 @@
+# exception handaling is the process of responding to the occurrence of exceptions – anomalous or exceptional conditions requiring special processing – during the execution of a program.
+
+
+a=input("Enter a number:")
+print(f"Multiply the table of{a}is")
+
+for i in range(1,11):
+   print(f"{int(a)} x {i}={int(a)*i}")
